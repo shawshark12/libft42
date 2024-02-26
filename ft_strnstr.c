cd7ca80f26b4c1294cac627a5bc82eea9701510a
Napath Pungkor <npungkor@z2t8c4.42bangkok.com>
2024-02-26 21:02:27 +0700

    feb26lastime

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npungkor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 18:18:31 by npungkor          #+#    #+#             */
+/*   Updated: 2024/02/26 21:01:44 by npungkor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (little[i] == 0)
+		return ((unsigned char *)big);
+	while (big[i] && i < len)
+	{
+		while (big[i + j] == little[j] && big[i + j] && i + j < len)
+		{
+			j++;
+			if (little[j] == 0)
+				return ((char *)big + i);
+		}
+		i++;
+	}
+	return (0);
+}
